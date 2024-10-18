3e1226f9ab7d6a98f4add8498072172cd6075bc3
GovindPatil75 <mulegr75@gmail.com>
2024-10-18 08:18:58 +0530

    added sample test

---
+++ src/main/java/com/Test/sampletest.java
@@ -0,0 +1,11 @@
+package com.Test;
+
+public class sampletest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//feature123
+		System.out.println("Sample test");
+	}
+
+}
